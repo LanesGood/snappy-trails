9447ca3813fdc232eac4b1d78509749049221699
LanesGood <Lane@developmentseed.org>
2023-06-09 22:15:46 -0400

    Create clearRouteLine function; add new map style... hide token

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -1,19 +1,15 @@
 import { DEFAULT_COORDS } from '../config';
 import { miliToTime } from '../helpers';
+const { MAPBOX_TOKEN } = process.env
 class MapView {
   // Leaflet objects and initialization
   constructor() {
-    this.map = L.map('map').setView([DEFAULT_COORDS[1], DEFAULT_COORDS[0]], 10);
-    this.Stamen_TonerLite = L.tileLayer(
-      'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-      'mapbox://styles/lanesgood/clhsi5wdt00yk01pffukxf1s4',
+    this.map = L.map('map').setView([DEFAULT_COORDS[1], DEFAULT_COORDS[0]], 5);
+    this.tiles = L.tileLayer(
+      `https://api.mapbox.com/styles/v1/lanesgood/clhsi5wdt00yk01pffukxf1s4/tiles/{z}/{x}/{y}?access_token=${MAPBOX_TOKEN}`,
       {
-        attribution:
-          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        subdomains: 'abcd',
         minZoom: 0,
-        maxZoom: 20,
-        ext: 'png',
+        maxZoom: 22,
       }
     );
     this.routeLine = L.polyline([]); // Set empty routeline to allow later clearing. Will likely later change to array of features to permit multiple lines
@@ -21,7 +17,7 @@ class MapView {
     this.currentPositionMarker = L.marker();
   }
   render() {
-    this.Stamen_TonerLite.addTo(this.map);
+    this.tiles.addTo(this.map);
     this.photoMarkers.addTo(this.map);
   }
   renderPhotoMarker(latitude, longitude, file, i) {
@@ -41,9 +37,11 @@ class MapView {
       coords[0],
     ]);
     const routeTime = miliToTime(routeData.paths[0].time);
-    console.log(routeData);
     this.routeLine.setLatLngs(routeCoords).addTo(this.map);
     this.routeLine.bindPopup(`${routeTime}`).openPopup();
+  }
+  clearRouteLine(){
+    this.routeLine = L.polyline([]);
   }
 }
 export default new MapView();
